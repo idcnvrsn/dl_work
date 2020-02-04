a1ed9a3437a9171c0f8f1cce5624d3a7504a04d4
idcnvrsn <idcnvrsn@gmail.com>
2020-02-04 22:34:48 +0900

    pascal　vocのデータセットを読み込むための処理を整理するため不要なコードを削除

--- DeepMetricLearning/L2_softmax/train.py
+++ DeepMetricLearning/L2_softmax/train.py
@@ -292,22 +292,6 @@ if __name__ == '__main__':
         y_ano = to_categorical(y_ano)
     
     else:
-        # 正常データ読み込み
-        image_filenames = glob(normal_image_dir+os.sep+"*.jpg")
-        image_filenames = image_filenames#[:1000]
-    
-        normal_images = np.zeros((len(image_filenames),image_file_size,image_file_size,3),dtype=np.uint8)
-        for index, image_filename in enumerate(image_filenames):
-            image = imageio.imread(image_filename, as_gray=False, pilmode="RGB")
-            image = cv2.resize(image,(image_file_size,image_file_size))
-            normal_images[index] = image
-        normal_images = normal_images.astype('float32') / 255
-    
-        y_normal = to_categorical([20]*len(normal_images))
-    
-        normal_train_images, normal_test_images, y_normal_train, y_normal_test = train_test_split(normal_images, y_normal, test_size=0.2, random_state=1)
-        normal_train_images, normal_val_images, y_normal_train, y_normal_val = train_test_split(normal_train_images, y_normal_train, test_size=0.2, random_state=1)
-
         ref_label_filenames = sorted(glob("VOCdevkit/VOC2012/ImageSets/Main/*_train.txt"))
         
         y_ref = np.zeros((5717,21),dtype=np.uint8)
@@ -329,19 +313,7 @@ if __name__ == '__main__':
         ref_images = ref_images.astype('float32') / 255
         ref_train_images, ref_test_images, y_ref_train, y_ref_test = train_test_split(ref_images, y_ref, test_size=0.2, random_state=1)
         ref_train_images, ref_val_images, y_ref_train, y_ref_val = train_test_split(ref_train_images, y_ref_train, test_size=0.2, random_state=1)
-    
-        # テストデータ(異常)読み込み
-        ano_image_filenames = glob(args.anomaly_image_dir+os.sep+"*.jpg")
-        ano_images = np.zeros((len(ano_image_filenames),image_file_size,image_file_size,3),dtype=np.uint8)
-        for index, filename in enumerate(ano_image_filenames):
-            image = imageio.imread(filename, as_gray=False, pilmode="RGB")
-            image = cv2.resize(image,(image_file_size,image_file_size))
-            ano_images[index] = image
-        
-        ano_images = ano_images.astype('float32') / 255
-        y_ano = to_categorical([20]*len(ano_images))
-        ano_val_images, ano_test_images, y_ano_val, y_ano_test = train_test_split(ano_images, y_ano, test_size=0.8, random_state=1)
-        
+            
     # 3種類のyについて幅を合わせる
     # normal
     y_zero = np.zeros((y_normal.shape[0],(y_ref.shape[1]+y_ano.shape[1]) ),dtype=np.float32)
